daca17f5b00cb21d71f5ea4ac59713b4db808c1b
Tom Starkey <127988404+TomStarkey@users.noreply.github.com>
2023-06-07 19:00:00 +0100

    Created script for about me questions; fix required: not showing media for banana.png

---
+++ switch.js
@@ -0,0 +1,12 @@
+function reveal(question_num, file_name,)
+{
+    let revealed = 0
+    if (revealed === 0){
+    pic = file_name
+    revealed = 1
+    }
+    else {
+    pic = "question_mark.png"
+    revealed = 0 
+    }
+}
