a062bd97206b0e87a1438011a7386d23e30e3c5d
Tom Starkey <127988404+TomStarkey@users.noreply.github.com>
2023-06-08 20:59:57 +0100

    Javascript function working, added q2

--- switch.js
+++ switch.js
@@ -1,12 +1,15 @@
-function reveal(question_num, file_name,)
+function reveal(question_num, file_name)
 {
-    let revealed = 0
     if (revealed === 0){
     pic = file_name
     revealed = 1
+    console.log(pic)
     }
     else {
     pic = "question_mark.png"
     revealed = 0 
+    console.log("Not the pic")
     }
+    document.getElementById(question_num).src = pic
 }
+let revealed = 0
